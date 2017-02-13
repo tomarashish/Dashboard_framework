bf0884ac9731f5bfaf68df7bd96d88ba747f283f
tomarashish <aashish.dyp@gmail.com>
2017-02-13 15:41:31 +0100

    add chart icon to sidebar

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,25 @@
+//https://tympanus.net/Development/DragDropInteractions/icons.html
+
+// variable to define custom grdistack behavior 
+var options = {
+    animate: true,
+    disableResize: false,
+    disableDrag: false,
+    resizable: {
+      handles: 'e, w'
+    }
+  };
+
+// using options variable to change gridstack behavior
+$('.grid-stack').gridstack(options);
+
+
+// use data-gs-id to get the change in data-gs-x and data-gs-y. Apply the change to adjacent grid to change the width in a responsive  way
+
+//set the min and max height of grid 
+// data-gs-min-width="1" data-gs-max-width="9" data-gs-min-height="1" data-gs-max-height="4" data-gs-id="1"
+
+
 $(document).ready(function () {
   var trigger = $('.hamburger'),
      isClosed = false;
@@ -23,20 +45,3 @@ $(document).ready(function () {
         $('#wrapper').toggleClass('toggled');
   });  
 });
-
-//https://tympanus.net/Development/DragDropInteractions/icons.html
-var options = {
-    animate: true,
-    disableResize: false,
-    disableDrag: false,
-    resizable: {
-      handles: 'e, w'
-    }
-  };
-
-$('.grid-stack').gridstack(options);
-
-// use data-gs-id to get the change in data-gs-x and data-gs-y. Apply the change to adjacent grid to change the width in a responsive  way
-
-//set the min and max height of grid 
-// data-gs-min-width="1" data-gs-max-width="9" data-gs-min-height="1" data-gs-max-height="4" data-gs-id="1"
